63832bf35fd6f2bfaed7e0786373a9e6f383c10b
Niroj Thapa <72069642+Niroj925@users.noreply.github.com>
2020-10-24 08:20:44 +0545

    Add files via upload

---
+++ nepal's map.c
@@ -0,0 +1,223 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("\n\n-------------------");
+	printf("---------------------");
+	printf("MAP OF NEPAL");
+	printf("---------------------");
+	printf("---------------------\n\n\n");
+	for(i=0;i<8;i++)
+	{
+		printf("  ");
+	}
+	printf(" * *");
+	printf("\n");
+	for(i=0;i<9;i++)
+	{
+		printf(" ");
+	}
+	printf("****   * * * *\n");
+	printf("           * * * * * * *\n");
+	printf("          ");
+	for(i=0;i<8;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("        ");
+	for(i=0;i<10;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("      ");
+	for(i=0;i<13;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("      ");
+	for(i=0;i<15;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("    ");
+	for(i=0;i<16;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("  ");
+	for(i=0;i<18;i++)
+	{
+		printf(" *");
+	}
+	printf("         *\n");
+	printf("  ");
+	for(i=0;i<20;i++)
+	{
+	printf(" *");
+	}
+	printf("   * * *\n");
+	for(i=0;i<25;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("  ");
+	for(i=0;i<24;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("     *  ");
+	for(i=0;i<21;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("          ");
+	for(i=0;i<21;i++)
+	{
+		printf(" *");
+	}
+	printf("    *\n");
+	printf("            ");
+	for(i=0;i<22;i++)
+	{
+		printf(" *");
+	}
+	printf("  *\n");
+	printf("              ");
+	for(i=0;i<22;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("                ");
+	for(i=0;i<22;i++)
+	{
+		printf(" *");
+	}
+	printf("   *\n");
+	printf("                  ");
+	for(i=0;i<24;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	printf("                    ");
+	for(i=0;i<23;i++)
+	{
+		printf(" *");
+	}
+	printf("      *\n");
+	printf("                      ");
+	for(i=0;i<23;i++)
+	{
+		printf(" *");
+	}
+	printf("   * *   **         * * *\n");
+	for(i=0;i<13;i++)
+	{
+		printf("  ");
+	}
+	for(i=0;i<34;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	for(i=0;i<14;i++)
+	{
+		printf("  ");
+	}
+	printf(" * * * * * * *    ");
+	for(i=0;i<24;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	for(i=0;i<16;i++)
+	{
+		printf("  ");
+	}
+	printf(" * *          ");
+	for(i=0;i<24;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	for(i=0;i<23;i++)
+	{
+		printf("  ");
+	}
+	for(i=0;i<23;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	for(i=0;i<24;i++)
+	{
+		printf("  ");
+	}
+	for(i=0;i<22;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	for(i=0;i<25;i++)
+	{
+		printf("  ");
+	}
+	for(i=0;i<20;i++)
+	{
+		printf(" *");
+	}
+	printf("*\n");
+	for(i=0;i<27;i++)
+	{
+		printf("  ");
+	}
+	for(i=0;i<19;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	for(i=0;i<28;i++)
+	{
+		printf("  ");
+	}
+	for(i=0;i<18;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	for(i=0;i<29;i++)
+	{
+		printf("  ");
+	}
+	printf(" * * *  ");
+	for(i=0;i<14;i++)
+	{
+		printf(" *");
+	}
+	printf("\n");
+	for(i=0;i<33;i++)
+	{
+		printf("  ");
+	}
+	printf(" * * * * * **  * * * * * * **\n");
+	for(i=0;i<34;i++)
+	{
+		printf("  ");
+	}
+	printf(" * * * * *   ");
+	printf(" * * * * * * *");
+	printf("\n\n\n");
+	printf("                               ");
+	printf("                               ");
+	printf("               By:-NIROJ THAPA-");
+	return 0;
+}
